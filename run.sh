520b9a4ac37a03d3c196d759e929dd5b41986b88
Manuel Bogner <manuel.bogner@openresearch.com>
2024-09-29 23:50:25 +0200

    run without intellij

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-python3 imagination.py --source ./tmp/source --target ./tmp/target --write_exif --copy_files
+#!/usr/bin/env bash
+source venv/bin/activate
+python3 imagination.py --source ./tmp/source --target ./tmp/target --write_exif --delete_duplicates
