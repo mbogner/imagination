d6e3a25b8bc6beab7424b370c34357c027f88d45
Manuel Bogner <manuel.bogner@openresearch.com>
2025-01-06 21:01:04 +0100

    threads and multiprocessing..

--- convert_to_jpegxl.py
+++ convert_to_jpegxl.py
@@ -79,7 +79,7 @@ def convert_to_jpegxl_task(args):
     source_path, target_path = args
     try:
         subprocess.run(
-            ["cjxl", source_path, target_path, "--quiet", "--lossless_jpeg=1", "--num_threads=2"],
+            ["cjxl", source_path, target_path, "--quiet", "--lossless_jpeg=1"],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
